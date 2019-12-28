4ebdd8f6e082f6aed6a156440a963f8910b90a14
Kyle Ferriter <kylerferriter@gmail.com>
2019-12-28 16:50:23 -0500

    Update AppExceptionHandler to properly return exception message

--- src/main/java/app/AppExceptionHandler.java
+++ src/main/java/app/AppExceptionHandler.java
@@ -6,25 +6,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
-import java.io.ByteArrayOutputStream;
 import java.io.PrintWriter;
-import java.io.UnsupportedEncodingException;
-import java.nio.charset.StandardCharsets;
+import java.io.StringWriter;
 
 @RestControllerAdvice
 public class AppExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Object> handleException(Exception e, HttpServletRequest request)
-            throws UnsupportedEncodingException {
-        //log.warn(e.getStackTrace());
-        ByteArrayOutputStream os = new ByteArrayOutputStream();
-
-        PrintWriter pw = new PrintWriter(os);
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleException(Exception e, HttpServletRequest request) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
         e.printStackTrace(pw);
-
         e.printStackTrace();
-        String stacktrace = os.toString(StandardCharsets.UTF_8.name());
+        String stacktrace = sw.toString();
         return new ResponseEntity<>(stacktrace, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
