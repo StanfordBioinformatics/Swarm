d815160402a2a99fce1897594f7f99abc6deb3b1
Amir Alavi <amiralavi7@gmail.com>
2021-03-04 14:45:05 -0500

    Remove dependency on Hummingbird

--- federated/setup.py
+++ federated/setup.py
@@ -4,6 +4,15 @@ setup(
     name='federated',
     packages=find_packages(),
     install_requires=[
-        "CloudHummingbird~=1.0.3"
+        'dsub==0.3.6',
+        'future==0.18.2',
+        'configparser==5.0.0',
+        'google-cloud-storage==1.30.0',
+        'boto3==1.14.38',
+        'azure-storage-blob==12.6.0',
+        'azure-identity==1.5.0',
+        'azure-batch==10.0.0',
+        'azure-mgmt-compute==18.0.0',
+        'retry==0.9.2',
     ]
 )
